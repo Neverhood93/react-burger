991e185162765b2c70031aa761d3bc55fe270aef
Neverhood93 <Neverhood93@gmail.com>
2024-04-13 21:00:23 +0300

    Новая версия header.

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,19 +1,26 @@
 import ButtonHeader from '../buttons/button-header/button-header';
 import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import styles from './app-header.module.css';
 
 export default function AppHeader() {
     return (
-        <header>
-            <ButtonHeader>
-                <BurgerIcon /><span>Конструктор</span>
-            </ButtonHeader>
-            <ButtonHeader>
-                <ListIcon  /><span>Лента заказов</span>
-            </ButtonHeader>
-            <Logo />
-            <ButtonHeader>
-                <ProfileIcon /><span>Лента заказов</span>
-            </ButtonHeader>
+        <header className={styles.header}>
+            <div className={styles.left_container}>
+                <ButtonHeader>
+                    <BurgerIcon /><span className="text text_type_main-medium">Конструктор</span>
+                </ButtonHeader>
+                <ButtonHeader>
+                    <ListIcon  /><span className="text text_type_main-medium">Лента заказов</span>
+                </ButtonHeader>
+            </div>
+            <div className={styles.logo}>
+                <Logo />
+            </div>
+            <div className={styles.right_container}>
+                <ButtonHeader>
+                    <ProfileIcon /><span className="text text_type_main-medium">Лента заказов</span>
+                </ButtonHeader>
+            </div>
         </header>        
     );
 }
--- src/components/buttons/button-header/button-header.js
+++ src/components/buttons/button-header/button-header.js
@@ -1,9 +1,9 @@
-
+import styles from './button-header.module.css';
 
 export default function ButtonHeader(props) {
     return (
-        <>
+        <button className={styles.button_header}>
             {props.children}
-        </>
+        </button>
     );
 }
