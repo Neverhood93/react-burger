78f958f0ccd6480e41be400cb13b6cd6f8076865
Neverhood93 <Neverhood93@gmail.com>
2024-05-19 15:20:05 +0300

    Добавлен роутер и первый маршрут для Home.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import AppHeader from "../app-header/app-header";
 import Home from "../../pages/home/home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <AppHeader />
-      <Home />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
