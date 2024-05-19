9d49280708faa4ab71bb3f92a9f02bf01a8a7b08
Neverhood93 <Neverhood93@gmail.com>
2024-05-19 23:20:49 +0300

    Сверстана ForgotPasswordPage без логики.

--- src/pages/forgot-password/forgot-password.tsx
+++ src/pages/forgot-password/forgot-password.tsx
@@ -1,7 +1,42 @@
+import {
+  Button,
+  Input,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import { Link } from "react-router-dom";
+import React from "react";
+import styles from "../common/form.module.css";
+
 function ForgotPasswordPage() {
   return (
-    <div>
-      <h1>ForgotPasswordPage</h1>
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <h2 className={"text text_type_main-medium mb-6"}>
+          Восстановление пароля
+        </h2>
+
+        <div className="mb-6">
+          <Input
+            type={"email"}
+            name={"email"}
+            placeholder={"Укажите e-mail"}
+            value={""}
+            onChange={() => {}}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+          />
+        </div>
+
+        <Button htmlType="button" type="primary" size="large">
+          Восстановить
+        </Button>
+      </form>
+      <p className={"text text_type_main-default text_color_inactive mt-20"}>
+        Вспомнили пароль?
+        <Link to="/login" className={styles.link}>
+          {" "}
+          Войти
+        </Link>
+      </p>
     </div>
   );
 }
