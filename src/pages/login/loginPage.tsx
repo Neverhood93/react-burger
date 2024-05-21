01b1ee5f7d76dbba312bca8bbb85cb6193458381
Neverhood93 <Neverhood93@gmail.com>
2024-05-21 20:05:14 +0300

    Реализован черновой вариант функционала страницы входа.

--- src/pages/login/loginPage.tsx
+++ src/pages/login/loginPage.tsx
@@ -3,26 +3,33 @@ import {
   Input,
   PasswordInput,
 } from "@ya.praktikum/react-developer-burger-ui-components";
-import React, { useEffect } from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "../common/form.module.css";
 import { useAppDispatch } from "../../services/hooks";
 import { login } from "../../services/auth/action";
+import { LoginRequest } from "../../types/types";
 
 function LoginPage() {
+  const [formState, setFormState] = useState<LoginRequest>({
+    email: "",
+    password: "",
+  });
+
   const dispatch = useAppDispatch();
-  useEffect(() => {
-    dispatch(
-      login({
-        email: "test234sdf234234@test.com",
-        password: "123345",
-      }),
-    );
-  }, []);
+
+  const handleFieldValueChanged = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    dispatch(login(formState));
+  };
 
   return (
     <div className={styles.container}>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <h2 className={"text text_type_main-medium mb-6"}>Вход</h2>
 
         <div className="mb-6">
@@ -30,18 +37,22 @@ function LoginPage() {
             type={"email"}
             name={"email"}
             placeholder={"E-mail"}
-            value={""}
-            onChange={() => {}}
+            value={formState.email}
+            onChange={handleFieldValueChanged}
             onPointerEnterCapture={undefined}
             onPointerLeaveCapture={undefined}
           />
         </div>
 
         <div className="mb-6">
-          <PasswordInput name={"password"} value={""} onChange={() => {}} />
+          <PasswordInput
+            name={"password"}
+            value={formState.password}
+            onChange={handleFieldValueChanged}
+          />
         </div>
 
-        <Button htmlType="button" type="primary" size="large">
+        <Button htmlType="submit" type="primary" size="large">
           Войти
         </Button>
       </form>
