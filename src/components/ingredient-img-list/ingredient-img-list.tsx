a734797e73b337d2d074a0db7280fd019b6ca807
Neverhood93 <Neverhood93@gmail.com>
2024-06-27 21:41:06 +0300

    Верстка карточки заказа в листе заказов.

---
+++ src/components/ingredient-img-list/ingredient-img-list.tsx
@@ -0,0 +1,41 @@
+import { useAppSelector } from "../../services/hooks";
+import { getIngredients } from "../../services/ingredients/selectors";
+import styles from "./ingredient-img-list.module.css";
+import React from "react";
+
+function IngredientImgList() {
+  const ingredients = useAppSelector(getIngredients);
+  const maxVisible = 5;
+  const hiddenCount = ingredients.length - maxVisible;
+
+  return (
+    <div className={styles.row}>
+      {ingredients.slice(0, maxVisible).map((item, index) => (
+        <div
+          key={index}
+          className={styles.ingredient_image}
+          style={{ zIndex: maxVisible - index }}
+        >
+          <img src={item.image_mobile} alt={item.name} />
+        </div>
+      ))}
+      {hiddenCount > 0 && (
+        <div
+          className={`${styles.ingredient_image} ${styles.hidden_ingredient}`}
+          style={{ zIndex: 0 }}
+        >
+          <img
+            src={ingredients[maxVisible].image_mobile}
+            alt={ingredients[maxVisible].name}
+            className={styles.hidden_image}
+          />
+          <span className={`text text_type_main-default ${styles.hidden_text}`}>
+            +{hiddenCount - 1}
+          </span>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default IngredientImgList;
--- src/components/order-card/order-card.tsx
+++ src/components/order-card/order-card.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import { IOrder } from "../../types/types";
 import styles from "./order-card.module.css";
 import { Link, useLocation } from "react-router-dom";
+import {
+  CurrencyIcon,
+  FormattedDate,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import IngredientImgList from "../ingredient-img-list/ingredient-img-list";
 
 const OrderCard: React.FC<IOrder> = ({ ...props }) => {
   const location = useLocation();
@@ -12,8 +17,32 @@ const OrderCard: React.FC<IOrder> = ({ ...props }) => {
       state={{ background: location }}
       className={styles.link}
     >
-      <div className={styles.card}>
-        <p>{props.number}</p>
+      <div className={styles.card_container}>
+        <div className={styles.card}>
+          <div className={styles.header_container}>
+            <div className={styles.order_number_container}>
+              <span
+                className={`text text_type_digits-medium ${styles.order_number}`}
+              >
+                #{props.number}
+              </span>
+            </div>
+            <p className="text text_type_main-default text_color_inactive">
+              <FormattedDate date={new Date(props.createdAt)} />
+            </p>
+          </div>
+
+          <p className="text text_type_main-medium mt-6">{props.name}</p>
+
+          <div className={styles.bottom_container}>
+            <IngredientImgList />
+
+            <div className={styles.total_price_container}>
+              <span className="text text_type_digits-default mr-2">{123}</span>
+              <CurrencyIcon type="primary" />
+            </div>
+          </div>
+        </div>
       </div>
     </Link>
   );
